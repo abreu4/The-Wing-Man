98cfbb49688e6f8ed22f7d3b5085031f9148c366
tiagoabreu <abreu4@users.noreply.github.com>
2020-02-23 11:12:51 +0100

    Crop to squares

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import ranker
 import utilities
 from swiper import Swiper
 from neural import Libido
-from data import convert, split
+from data import convert, split, crop_to_squares
 
 # TODO: - Test routine to extract only pictures with people
+# TODO: - Needs additional crop_to_square routine - add to data.py - in order to train model
 
 
 DATA_TEST = 'data/testing'
@@ -22,16 +23,28 @@ DATA_SORTED = 'data/sorted'
 
 def main():
 
+    mode = "SD"
+
+    """ DATA TREATMENT 1
+    neural_paths = {x : os.path.join(DATA_SORTED, x) for x in ["train", "test"]}
+    for phase in ["train", "test"]:
+        crop_to_squares(os.path.join(neural_paths[phase], "left"))
+        crop_to_squares(os.path.join(neural_paths[phase], "right"))
+    exit()
     """
+
+    """ DATA TREATMENT 2
     convert(os.path.join(DATA_SORTED, "left"))
     convert(os.path.join(DATA_SORTED, "right"))
 
     split(DATA_SORTED, 0.85)
     """
 
+    # TRAINING
     predictor = Libido()
     predictor.train_model(pretrained=True)
     exit()
+
 
     # testing the 'swiper.py'
     swiper = Swiper()
